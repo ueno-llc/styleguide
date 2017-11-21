41c9189a1ce4e71b9d5032b8b7dbc3b4f3a986fe
Jérémy Barbet <jeremgraph@gmail.com>
2017-11-21 14:34:14 +0000

    Improve react-native eslint config

--- packages/eslint-config/react-native.js
+++ packages/eslint-config/react-native.js
@@ -7,14 +7,24 @@ module.exports = {
     require.resolve('./base/index'),
   ],
   plugins: [
+    'react',
     'react-native',
   ],
   rules: {
+    // Can't declare custom path to components directory
+    'import/no-extraneous-dependencies': 0,
+
     // React Native's bundler is weird
     'import/no-unresolved': 0,
 
     // We define stylesheets below component classes
     'no-use-before-define': 0,
+
+    // This rule will find variables used in JSX and mark them as used.
+    'react/jsx-uses-vars': 1,
+
+    // JSX should consider the React variable as used.
+    'react/jsx-uses-react': 1,
 
     // Add React Native-specific rules
     'react-native/no-unused-styles': 0,

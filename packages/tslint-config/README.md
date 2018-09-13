00896c06a6e7ca8d908114cb810776d469743fc3
Jérémy Barbet <jeremgraph@gmail.com>
2018-09-13 09:32:55 +0000

    First version of tsconfig for react-native

---
+++ packages/tslint-config/README.md
@@ -0,0 +1,52 @@
+# @ueno/tslint-config
+
+> Ueno's tslint config
+
+[![npm](https://img.shields.io/npm/v/@ueno/tslint-config.svg)](https://www.npmjs.com/package/@ueno/tslint-config)
+
+## Installation
+
+```bash
+yarn add @ueno/tslint-config
+```
+
+## Usage
+
+We have 3 differents configs, reference the one you need in your `.tslint.json`
+
+**For Vanilla.js** (Not done yet)
+
+```json
+{
+  "extends": "@ueno/tslint-config/vanilla"
+}
+```
+
+**For React.js** (Not done yet)
+
+```json
+{
+  "extends": "@ueno/tslint-config/react"
+}
+```
+
+**For React-native**
+
+```json
+{
+  "extends": "@ueno/tslint-config/react-native"
+}
+```
+
+## Local Development
+
+```bash
+git clone https://github.com/ueno-llc/styleguide
+cd styleguide/packages/tslint-config
+yarn install
+yarn test
+```
+
+## License
+
+MIT &copy; [ueno.](http://ueno.co)
---
+++ packages/tslint-config/react-native.js
@@ -0,0 +1,62 @@
+const path = require('path');
+
+module.exports = {
+  rulesDirectory: [
+    path.join(path.dirname(require.resolve('tslint-microsoft-contrib')), './'),
+    path.join(path.dirname(require.resolve('tslint-origin-ordered-imports-rule')), './'),
+  ],
+  rules: {
+    quotemark: [
+      true,
+      'single',
+      'jsx-double',
+    ],
+
+    'origin-ordered-imports': [
+      true,
+      'one-blank-line',
+    ],
+
+    'max-line-length': [
+      true,
+      {
+        limit: 140,
+        'ignore-pattern': '^import |^export {(.*?)}',
+      },
+    ],
+
+    'no-implicit-dependencies': [
+      true,
+      'dev',
+      [
+        '~assets',
+        '~components',
+        '~config',
+        '~stores',
+        '~utils',
+        '~screens',
+      ],
+    ],
+
+    'variable-name': [
+      true,
+      'allow-pascal-case',
+      'ban-keywords',
+      'check-format',
+    ],
+
+    typedef: false,
+
+    'no-submodule-imports': false,
+    'ordered-imports': false,
+    'import-name': false,
+    'export-name': false,
+    'no-relative-imports': false,
+    'trailing-comma': true,
+    'missing-jsdoc': false,
+    'completed-docs': false,
+    'no-unsafe-any': false,
+    'no-floating-promises': false,
+    'strict-boolean-expressions': false,
+  },
+};

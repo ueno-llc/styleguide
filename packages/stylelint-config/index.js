736271bf31dc8e14f1317ca52ba18268154b3d75
Jérémy Barbet <jeremgraph@gmail.com>
2019-02-21 12:21:59 +0000

    Update stylelint config

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -18,12 +18,13 @@ module.exports = {
             'content',
           ],
         },
+
         {
           order: 'flexible',
           properties: [
             'display',
-            'float',
-            'clear',
+
+            // display flex properties
             'flex',
             'flex-grow',
             'flex-shrink',
@@ -31,10 +32,13 @@ module.exports = {
             'flex-direction',
             'flex-wrap',
             'flex-flow',
+            'align-content',
             'align-items',
             'align-self',
             'justify-content',
             'order',
+
+            // Grid properties
             'grid',
             'grid-area',
             'grid-auto-columns',
@@ -53,8 +57,13 @@ module.exports = {
             'grid-template-areas',
             'grid-template-columns',
             'grid-template-rows',
+
+            // Old days
+            'float',
+            'clear',
           ],
         },
+
         {
           order: 'flexible',
           properties: [
@@ -66,13 +75,7 @@ module.exports = {
             'z-index',
           ],
         },
-        {
-          order: 'flexible',
-          properties: [
-            'visibility',
-            'opacity',
-          ],
-        },
+
         {
           order: 'flexible',
           properties: [
@@ -88,6 +91,7 @@ module.exports = {
             'padding-left',
           ],
         },
+
         {
           order: 'flexible',
           properties: [
@@ -97,42 +101,20 @@ module.exports = {
             'height',
             'max-height',
             'min-height',
+          ],
+        },
+
+        {
+          order: 'flexible',
+          properties: [
+            'visibility',
             'overflow',
           ],
         },
+
         {
           order: 'flexible',
           properties: [
-            'font',
-            'font-size',
-            'font-weight',
-            'font-style',
-            'font-family',
-            'font-variant',
-            'font-feature',
-            'font-kerning',
-            'font-stretch',
-            'line-height',
-            'color',
-            'text',
-            'text-align',
-            'letter-spacing',
-            'white-space',
-            'vertical-align',
-          ],
-        },
-        {
-          order: 'flexible',
-          properties: [
-            'cursor',
-            'user-select',
-            'pointer-events',
-          ],
-        },
-        {
-          order: 'flexible',
-          properties: [
-            'background',
             'border',
             'border-top',
             'border-right',
@@ -145,7 +127,102 @@ module.exports = {
             'border-bottom-left-radius',
           ],
         },
+
+        {
+          order: 'flexible',
+          properties: [
+            'cursor',
+            'user-select',
+            'pointer-events',
+          ],
+        },
+
+        {
+          order: 'flexible',
+          properties: [
+            'font',
+            'font-family',
+            'font-feature-settings',
+            'font-kerning',
+            'font-size',
+            'font-stretch',
+            'font-style',
+            'font-variant',
+            'font-variant-caps',
+            'font-variant-east-asian',
+            'font-variant-ligatures',
+            'font-variant-numeric',
+            'font-variation-settings',
+            'font-weight',
+            '-webkit-font-size-delta',
+            '-webkit-font-smoothing',
+
+            'line-break',
+            'line-height',
+
+            'color',
+
+            'text-align',
+            'text-align-last',
+            'text-combine-upright',
+            'text-decoration',
+            'text-decoration-color',
+            'text-decoration-line',
+            'text-decoration-skip-ink',
+            'text-decoration-style',
+            'text-indent',
+            'text-orientation',
+            'text-overflow',
+            'text-rendering',
+            'text-shadow',
+            'text-size-adjust',
+            'text-transform',
+            'text-underline-position',
+
+            'letter-spacing',
+            'white-space',
+            'vertical-align',
+          ],
+        },
+
+        {
+          order: 'flexible',
+          properties: [
+            'background',
+            'background-attachment',
+            'background-blend-mode',
+            'background-clip',
+            'background-color',
+            'background-image',
+            'background-origin',
+            'background-position',
+            'background-position-x',
+            'background-position-y',
+            'background-repeat',
+            'background-repeat-x',
+            'background-repeat-y',
+            'background-size',
+            'opacity',
+          ],
+        },
       ],
+
+      {
+        order: 'flexible',
+        properties: [
+          'transform',
+          'transform-box',
+          'transform-origin',
+          'transform-style',
+
+          'transition',
+          'transition-delay',
+          'transition-duration',
+          'transition-property',
+          'transition-timing-function',
+        ],
+      },
+
       {
         unspecified: 'bottom',
       },
